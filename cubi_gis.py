e06f39187126f61cbbb2ea50d1b8bf19995eeebb
Justin Hanley <jhanley@arrowheadcu.org>
2016-03-31 08:07:26 -0700

    Alter cubi_gis.GIS().geocode_address geocoder parameter to accept list of geocoders rather than individual. Geocoded addresses update return dictionary with all available information provided by geocoding services.

--- cubi_gis.py
+++ cubi_gis.py
@@ -24,66 +24,69 @@ class GIS(object):
         """
     
 
-    def geocode_address(self, address, geocoder):
+    def geocode_address(self, address, geocoder_list):
         """Geocode a given address with a geocode service.         
         :Paramters:
             - 'address': A single string represented address. Paramter is scrubbed on client, in addition
                to any geocode service processing.
-            - 'geocoder': A string represented geocoding service provider. See geopy  documentation at http://geopy.readthedocs.org/en/latest/
+            - 'geocoder_list': A list of string represented geocoding service providers. See geopy documentation at http://geopy.readthedocs.org/en/latest/
                for list of geocode services.
 
         Method returns dictionary of address items from geocode services. Raw
         output will differ between service API's
         """
-        
-        # Create geocoder with string object and find address from service
-        geolocator = geopy.geocoders.get_geocoder_for_service(geocoder)
+
         address = re.sub(r'\$|#|"|:', ' ', address)
-        location = geolocator().geocode(address)    
-        
-        # Flatten raw return data for normalization. This must be manually mapped otherwise filed will return None
-        try:
-            if geolocator.__name__ == 'ArcGIS':
-                mapped_items = {
-                    'longitude':location.raw['feature']['geometry']['x']
-                    ,'latitude': location.raw['feature']['geometry']['y']
-                    ,'geocode_address_type': location.raw['feature']['attributes']['Addr_Type']
-                    ,'geocode_score': location.raw['feature']['attributes']['Score']
-                    ,'max_latitude': location.raw['extent']['ymax']
-                    ,'min_latitude': location.raw['extent']['ymin']
-                    ,'max_longitude': location.raw['extent']['xmax']
-                    ,'min_longitude': location.raw['extent']['xmin']
-                    ,'return_address': location.raw['name']
-                }
-            elif geolocator.__name__ == 'Nominatim':
-                mapped_items = {
-                    'longitude':location.raw['lon']
-                    ,'latitude': location.raw['lat']
-                    ,'geocode_address_type': location.raw['type']
-                    ,'geocode_score': location.raw['importance']
-                    ,'max_latitude': location.raw['boundingbox'][1]
-                    ,'min_latitude': location.raw['boundingbox'][0]
-                    ,'max_longitude': location.raw['boundingbox'][3]
-                    ,'min_longitude': location.raw['boundingbox'][2]
-                    ,'return_address': location.raw['display_name']
-                }
-            else:
-                mapped_items = None
-        except:
-            mapped_items = None
-            raise
-        
         # Build dictionary of values. Structure may need to be reconsidered for noSQL optimization
         addresses = {
             'address': address,
-            'geolocation': [
-                {
-                    'geolocator': '{0}.{1}'.format(geolocator().__module__, geolocator.__name__),
+            'geolocation': []
+        }
+
+        for geocoder in geocoder_list:
+            # Create geocoder with string object and find address from service
+            geolocator = geopy.geocoders.get_geocoder_for_service(geocoder)
+            location = geolocator().geocode(address)    
+            
+            # Flatten raw return data for normalization. This must be manually mapped otherwise filed will return None
+            try:
+                if geolocator.__name__ == 'ArcGIS':
+                    mapped_items = {
+                        'longitude':location.raw['feature']['geometry']['x']
+                        ,'latitude': location.raw['feature']['geometry']['y']
+                        ,'geocode_address_type': location.raw['feature']['attributes']['Addr_Type']
+                        ,'geocode_score': location.raw['feature']['attributes']['Score']
+                        ,'max_latitude': location.raw['extent']['ymax']
+                        ,'min_latitude': location.raw['extent']['ymin']
+                        ,'max_longitude': location.raw['extent']['xmax']
+                        ,'min_longitude': location.raw['extent']['xmin']
+                        ,'return_address': location.raw['name']
+                    }
+                elif geolocator.__name__ == 'Nominatim':
+                    mapped_items = {
+                        'longitude':location.raw['lon']
+                        ,'latitude': location.raw['lat']
+                        ,'geocode_address_type': location.raw['type']
+                        ,'geocode_score': location.raw['importance']
+                        ,'max_latitude': location.raw['boundingbox'][1]
+                        ,'min_latitude': location.raw['boundingbox'][0]
+                        ,'max_longitude': location.raw['boundingbox'][3]
+                        ,'min_longitude': location.raw['boundingbox'][2]
+                        ,'return_address': location.raw['display_name']
+                    }
+                else:
+                    mapped_items = None
+            except:
+                mapped_items = None
+                raise
+
+            addresses['geolocation'].append(
+                   {'geolocator': '{0}.{1}'.format(geolocator().__module__, geolocator.__name__),
                     'geocode_datetime': datetime.date.today(),
                     'flat_return': mapped_items,
                     'raw_return' : location.raw,
-                },
-            ]
-        }
+                    }
+                   )
+        
 
         return addresses
